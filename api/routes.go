7a5671fe91b7e7423d4b6aee442985d64593976a
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-10-15 16:15:11 -0300

    refactor: routes path

---
+++ api/routes.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hjunior29/go-tasker/api/handlers"
+)
+
+// SetupRoutes defines the API routes.
+func SetupRoutes() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", api.HomeQueueHandler)
+	r.GET("/info", api.InfoQueueHandler)
+	r.POST("/enqueue", api.EnqueueTaskHandler)
+
+	return r
+}

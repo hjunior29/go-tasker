55848406ff0eef322576e1faeb52cdbbeaaebeea
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-10-29 00:52:05 -0300

    feat: adding scheduler options for tasks

--- models/tasks_request.go
+++ models/tasks_request.go
@@ -2,7 +2,8 @@ package models
 
 // TasksRequest represents the structure of a TasksRequest for processing.
 type TasksRequest struct {
-	URL     string                 `json:"url"`
-	Method  string                 `json:"method"`
-	Payload map[string]interface{} `json:"payload"`
+	URL         string                 `json:"url"`
+	Method      string                 `json:"method"`
+	ScheduledAt int                    `json:"scheduled_at"`
+	Payload     map[string]interface{} `json:"payload"`
 }
